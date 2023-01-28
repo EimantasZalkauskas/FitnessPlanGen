7db9969050fc2e9d87c53c5c5c8be6aba8529b46
Eimantas <eimantaszalkauskas@gmail.com>
2023-01-28 11:44:35 +0000

    Updated DB connection

--- MuscleGroup.py
+++ MuscleGroup.py
@@ -1,4 +1,4 @@
-
+import psycopg2
 
 class MuscleGroupClass():
 
@@ -8,6 +8,9 @@ class MuscleGroupClass():
         self.split_category = None
         self.essential_weighting = None
         self.difficulty = None
+        self.myConnection = None
+        self.query_get_id = """SELECT "ExerciseID" FROM  "ExerciseInfo" WHERE "Difficulty" = 'Low' OR "Difficulty" = 'Mid'; """
+        self.query_get_all_from_id = """SELECT * FROM  "ExerciseInfo" WHERE "ExerciseID" = '{}' """
     
     def db_connect(self):
         hostname = 'db.bit.io'
@@ -15,21 +18,36 @@ class MuscleGroupClass():
         password = 'v2_3yLM5_pziuTrAAykrhRKCZeTWC7mS'
         database = 'EimantasZ/FitnessDb'
 
-        print( "Using psycopg2:" )
-        import psycopg2
-        myConnection = psycopg2.connect( host=hostname, user=username, password=password, dbname=database )
-        self.doQuery( myConnection )
-        myConnection.close()
+        self.myConnection = psycopg2.connect( host=hostname, user=username, password=password, dbname=database )
+    
+    def db_connection_close(self):
+        self.myConnection.close()
 
         # Simple routine to run a query on a database and print the results:
-    def doQuery(self, conn ) :
+    def queryReturnArrStringOfIds(self, conn, query) :
+        arr = []
+        string = ''
+        cur = conn.cursor()
+        cur.execute(query)
+        
+        for item in cur.fetchall() :
+            string = ''.join(map(str,item)) 
+            arr.append(string)
+
+        #print(arr)
+        return arr
+    
+    def queryReturnAllDataFromTable(self, conn, query, arr_id):
         cur = conn.cursor()
 
-        cur.execute("""SELECT * FROM  "MuscleGroup"; """)
-
-        for item in cur.fetchall() :
-            print( item )
-
+        for item in arr_id:
+            cur.execute(query.format(item))
+            for row in cur.fetchall():
+                print(row)
 
 inst = MuscleGroupClass()
 inst.db_connect()
+arr = inst.queryReturnArrStringOfIds(inst.myConnection, inst.query_get_id)
+inst.queryReturnAllDataFromTable(inst.myConnection, inst.query_get_all_from_id, arr)
+inst.db_connection_close()
+#print(arr)
